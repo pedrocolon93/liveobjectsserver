698d01e3b69c6f40abd61519a8535d9d5e7e35db
Arata Miyamoto <artimo14@Aratas-MacBook-Pro.local>
2015-02-09 14:53:31 -0500

    use express

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var http = require("http"),
-    server;
+	express = require("express"),
+    app = express();
 
-server = http.createServe(function (req, res) {
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.end("My Hello World!");
+app.use(express.static(__dirname + "/client"));
+
+http.createServer(app).listen(3000);
+
+app.get("/hello", function (req, res) {
+	res.send("Hello World!");
 });
 
-server.listen(3000);
-
-console.log("Server listening on port 3000");
+app.get("/goodbye", function (req, res) {
+	res.send("Goodbye World!");
+});
