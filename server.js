2780369bba216bb4965eab30122ff64dc3f878f3
Arata Miyamoto <artimo14@Aratas-MacBook-Pro.local>
2015-02-09 16:46:53 -0500

    check file existence

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var http = require("http"),
 	express = require("express"),
+	url = require("url"),
+	path = require("path"),
     app = express();
 
 http.createServer(app).listen(3000);
 
 app.get("/*", function (req, res) {
+	var uri = url.parse(req.url).pathname;
+	var filePath = path.join(process.cwd(), "storage", uri);
 
-	res.send("request url: " + req.url.toString());
+	path.exists(filePath, function(exists) {
+		if (exists) {
+			res.send("file exists " + filePath);
+		} else {
+			res.send("file doesn't exist " + filePath);			
+		}
+	})
+
 });
