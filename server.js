28359ad0ad628d1ea8b924834d20fe21766fd49c
Arata Miyamoto <artimo14@Aratas-MacBook-Pro.local>
2015-02-09 20:50:27 -0500

    fix an error

--- server.js
+++ server.js
@@ -7,38 +7,32 @@ var http = require("http"),
 
 http.createServer(app).listen(3000);
 
-var fileGetCallback = new function (req, res) {
+var fileGetCallback = function (req, res) {
 	var uri = url.parse(req.url).pathname;
 	var filePath = path.join(process.cwd(), "storage", uri);
 
-	fs.exists(filePath, function(exists) {
-		if (exists) {
-			fs.stat(filePath, function (err, stats) {
+	fs.stat(filePath, function (err, stats) {
+		if (err) {
+			if (err.code == "NOENT") {
+				res.send("file doesn't exist " + filePath);
+			} else {
+				console.log(err);
+			}
+		} else if (stats.isFile()) {
+			res.sendFile(filePath, null, function (err) {
 				if (err) {
 					console.log(err);
-				}
-
-				if (stats.isFile()) {
-					res.send("the file is a file " + filePath);
-					res.sendFile(fileName, options, function (err) {
-						if (err) {
-							console.log(err);
-							res.status(err.status).end();
-						} else {
-							console.log("Sent:", filePath);
-						}
-					})
-				} else if (stats.isDirectory()) {
-					res.send("the file is a directory " + filePath);
+					res.status(err.status).end();
 				} else {
-					res.send("unknown file type " + filePath);
+					console.log("Sent:", filePath);
 				}
 			});
+		} else if (stats.isDirectory()) {
+			res.send("the file is a directory " + filePath);
 		} else {
-			res.send("file doesn't exist " + filePath);			
+			res.send("unknown file type " + filePath);
 		}
-		})	
-}
+	});
+};
 
 app.get("/*", fileGetCallback);
-}
