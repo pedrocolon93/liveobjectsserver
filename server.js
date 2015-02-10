e53361d3abd5a1b322b3754ae61ac713ce03e629
Arata Miyamoto <artimo14@Aratas-MacBook-Pro.local>
2015-02-09 20:05:11 -0500

    check if the specified file exists on the server

--- server.js
+++ server.js
@@ -2,20 +2,43 @@ var http = require("http"),
 	express = require("express"),
 	url = require("url"),
 	path = require("path"),
+	fs = require("fs"),
     app = express();
 
 http.createServer(app).listen(3000);
 
-app.get("/*", function (req, res) {
+var fileGetCallback = new function (req, res) {
 	var uri = url.parse(req.url).pathname;
 	var filePath = path.join(process.cwd(), "storage", uri);
 
-	path.exists(filePath, function(exists) {
+	fs.exists(filePath, function(exists) {
 		if (exists) {
-			res.send("file exists " + filePath);
+			fs.stat(filePath, function (err, stats) {
+				if (err) {
+					console.log(err);
+				}
+
+				if (stats.isFile()) {
+					res.send("the file is a file " + filePath);
+					res.sendFile(fileName, options, function (err) {
+						if (err) {
+							console.log(err);
+							res.status(err.status).end();
+						} else {
+							console.log("Sent:", filePath);
+						}
+					})
+				} else if (stats.isDirectory()) {
+					res.send("the file is a directory " + filePath);
+				} else {
+					res.send("unknown file type " + filePath);
+				}
+			});
 		} else {
 			res.send("file doesn't exist " + filePath);			
 		}
-	})
+		})	
+}
 
-});
+app.get("/*", fileGetCallback);
+}
