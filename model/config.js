7cad4b1ae3343e12f601c9136337c1f1cfb28686
Live Object <liveobj@example.com>
2015-02-19 21:40:55 +0000

    add APPCHANNEL to the config model

--- model/config.js
+++ model/config.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 
 exports.connect = function (callback) {
-	mongoose.connect('mongodb://localhost/flashair-compat-server');
-	mongoose.connection.on("open", function () {
-		exports.Config = mongoose.model("Config", exports.ConfigSchema);
+    mongoose.connect('mongodb://localhost/flashair-compat-server');
+    mongoose.connection.on("open", function () {
+        exports.Config = mongoose.model("Config", exports.ConfigSchema);
 
-		callback();
-	});
+        callback();
+    });
 }
 
 exports.getConfig = function (callback) {
@@ -22,25 +22,27 @@ exports.getConfig = function (callback) {
 }
 
 exports.ConfigSchema = mongoose.Schema({
-	APPAUTOTIME: 	Number,
-	APPINFO: String,
-	APPMODE: { type: Number, default: 4 },
-	APPNAME: { type: String, default: 'myflashair-compat' },
-	APPNETWORKKEY: { type: String, default: '12345678' },
-	APPSSID: { type: String, default: 'flashair-compat' },
-	BRGNETWORKKEY: String,
-	BRGSSID: String,
-	CID: String,
-	CIPATH: String,
-	DELCGI: String,
-	DNSMODE: Number,
-	IFMODE: Number,
-	LOCK: Number,
-	MASTERCODE: String,
-	NOISE_CANCEL: Number,
-	PRODUCT: String,
-	UPDIR: String,
-	UPLOAD: Number,
-	VENDOR: { type: String, default: 'MIT Media Lab' },
-	VERSION: String
+    APPAUTOTIME: Number,
+    APPINFO: String,
+    APPMODE: { type: Number, default: 4 },
+    APPNAME: { type: String, default: 'myflashair-compat' },
+    APPNETWORKKEY: { type: String, default: '12345678' },
+    APPSSID: { type: String, default: 'flashair-compat' },
+    BRGNETWORKKEY: String,
+    BRGSSID: String,
+    CID: String,
+    CIPATH: String,
+    DELCGI: String,
+    DNSMODE: Number,
+    IFMODE: Number,
+    LOCK: Number,
+    MASTERCODE: String,
+    NOISE_CANCEL: Number,
+    PRODUCT: String,
+    UPDIR: String,
+    UPLOAD: Number,
+    VENDOR: { type: String, default: 'MIT Media Lab' },
+    VERSION: String,
+
+    APPCHANNEL: { type: Number, min: 0, max: 14 }
 });
